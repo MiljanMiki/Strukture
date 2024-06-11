5f9cbc09e318191b79010fbf75eb5c70001d5d5d
Miljan <miljan.dragovic@elfak.rs>
2024-06-11 16:56:24 +0200

    Dodat zadatak da put mora da prodje kroz zadati cvor (doradi ga)

---
+++ Vezbanje II kol/Lab04/JovBogdnZad/CelobrojniGraf/CelobrojniGraf/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+#include "GraphInt.h"
+
+int main(int argc, char* argv[]) 
+{
+	GraphAsListsInt graf;
+
+	/*
+	1------------------>5
+	| \					^				
+	|   \				|
+	|    \				|
+	|     \				|
+	2      6			|
+	 \      \			|
+	  \      \			|
+	   \      \			|
+	    \      \		|
+		 3----->4-------|
+	*/
+
+	//tipa za ovaj primer sve radi normalno
+	/*graf.insertNode(1);
+	graf.insertNode(5);
+	graf.insertNode(2);
+	graf.insertNode(3);
+	graf.insertNode(4);
+	graf.insertNode(6);
+
+	graf.insertEdge(1, 5);
+	graf.insertEdge(1, 2);
+	graf.insertEdge(1, 6);
+	graf.insertEdge(2, 3);
+	graf.insertEdge(3, 4);
+	graf.insertEdge(4, 5);
+	graf.insertEdge(6, 4);*/
+
+	//za ovo mi vec ne radi (ponovo ne stampa kako valja...)
+	graf.insertNode(1); graf.insertNode(2); graf.insertNode(3); graf.insertNode(4);
+	graf.insertNode(5); graf.insertNode(6); graf.insertNode(7); graf.insertNode(8);
+
+	graf.insertEdge(1, 2);
+	graf.insertEdge(1, 4);
+	graf.insertEdge(2, 3);
+	graf.insertEdge(2, 4);
+	graf.insertEdge(3, 1);
+	graf.insertEdge(4, 5);
+	graf.insertEdge(4, 6);
+	graf.insertEdge(5, 7);
+	graf.insertEdge(6, 2);
+	graf.insertEdge(6, 5);
+	graf.insertEdge(6, 7);
+	graf.insertEdge(7, 8);
+
+	//graf.print();
+	graf.pathThroughNode(6,4,1);
+}
+
